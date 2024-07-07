5f7dee6fb54723990cd2e368761ab6ee79d9f2f2
mengxinajin <mxj199012@126.com>
2024-07-07 19:24:56 +0800

    Add files via upload
    初始化

---
+++ Test_package/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Test_package
+//
+//  Created by 孟现进 on 2024/6/30.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Test_package/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Test_package
+//
+//  Created by 孟现进 on 2024/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+        
+        
+        
+    }
+
+
+}
+
